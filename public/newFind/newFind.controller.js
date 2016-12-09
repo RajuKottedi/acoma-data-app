09d19922044ae99ad9e131bb5d649671fb83d3f5
Emily Kauffman <kauffmanes@gmail.com>
2016-12-09 09:24:27 -0500

    added latlng

--- public/newFind/newFind.controller.js
+++ public/newFind/newFind.controller.js
@@ -10,32 +10,43 @@ angular.module('app')
 
 			$scope.formData = {};
 
+			$scope.location = {
+				lat: '',
+				lng: ''
+			};
+
 			$scope.data = [{
 				sectionTitle: 'Find/Site Information',
 				expandedDisplay: false,
 				questions: [{
-					model: 'site',
+					id: 'site',
 					label: 'Site',
 					description: '',
 					inputType: 'text',
 					disabled: true,
 					value: 'Example Site Name',
 				}, {
-					model: null,
+					id: null,
 					label: 'Latitude',
 					description: '',
 					inputType: 'text',
 					disabled: true,
 					value: '',
+					placeholder: 'Fetching latitude...'
 				}, {
-					model: null,
-					label: 'Longitude'
+					id: null,
+					label: 'Longitude',
+					description: '',
+					inputType: 'text',
+					disabled: true,
+					value: '',
+					placeholder: 'Fetching longitude...'
 				}]
 			}, {
 				sectionTitle: 'Variables measured on all body sherds',
 				expandedDisplay: true,
 				questions: [{
-					model: null,
+					id: null,
 					label: 'Clay Color/Ware',
 					description: 'A sub-sample from each site will be coded by me using Munsell charts.  This variable is notincluded in the quantitative analyses.',
 					inputType: 'select',
@@ -50,7 +61,7 @@ angular.module('app')
 						{ label: '99 - other, specify in notes', value: 99 }
 					]
 				}, {
-					model: null,
+					id: null,
 					label: 'Vessel Portion',
 					description: '',
 					inputType: 'select',
@@ -68,7 +79,7 @@ angular.module('app')
 						{ label: '99 - other, specify in notes', value: 99 }
 					]
 				}, {
-					model: null,
+					id: null,
 					label: 'Type (primary treatment)',
 					description: 'A majority of sherds will fit into one of these categories.  If a sherd does not fit into any of these categories, code it as “other” and write a description in the notes field.  These variables arenot included in the quantitative analyses.',
 					inputType: 'select',
@@ -87,7 +98,7 @@ angular.module('app')
 						{ label: '99 - other, specify in notes', value: 99 }
 					]
 				}, {
-					model: null,
+					id: null,
 					label: 'Type of indentations',
 					description: 'This category only applies to indented, zoned, and patterned corrugated sherds.',
 					inputType: 'select',
@@ -101,7 +112,7 @@ angular.module('app')
 						{ label: '99 - other, specify in notes', value: 99 }
 					]
 				}, {
-					model: null,
+					id: null,
 					label: 'Direction of indentations',
 					description: 'This variable relates to the direction of the indentations in relation to the coils used to form thevessel.  As with the variable above, this is only measured on indented, zoned, and patterned corrugated sherds.  See example sherds.',
 					inputType: 'select',
@@ -115,7 +126,7 @@ angular.module('app')
 						{ label: '3 – oblique – the indentation is between parallel/perpendicular', value: 3 }
 					]
 				}, {
-					model: null,
+					id: null,
 					label: 'Indentation Alignment',
 					description: 'This variable relates to whether or not indentations are aligned between coils. This variable is only measured on indented, zoned, and patterned corrugated sherds.',
 					inputType: 'select',
@@ -129,7 +140,7 @@ angular.module('app')
 						{ label: '3 – diagonally aligned – indentation clearly diagonally aligned', value: 3 }
 					]
 				}, {
-					model: null,
+					id: null,
 					label: 'Type of surface elaborations',
 					description: 'This variable refers secondary surface elaborations that are applied after the vessel is formed. The list below includes most of the secondary surface treatments that you are likely to encounter. If you encounter a surface treatment not included here, code this sherd as “other” and write a description in the notes field.',
 					inputType: 'select',
@@ -144,7 +155,7 @@ angular.module('app')
 						{ label: '99 – other/multiple – specify in the notes', value: 99 }
 					]
 				}, {
-					model: null,
+					id: null,
 					label: 'Vessel form',
 					description: 'The type of vessel. Use one of the following categories. If a sherd does not fit any of these categories, code it as “other” and describe it in the notes field.',
 					inputType: 'select',
@@ -163,7 +174,7 @@ angular.module('app')
 						{ label: '99 – other - specify in the notes', value: 99 }
 					]
 				}, {
-					model: null,
+					id: null,
 					label: 'Presence/absence of smudging',
 					description: 'This variable refers to the presence or absence of smudging. Smudging is most common on the interior surface of bowls. It characterized as a black, waxy feeling, and reflective surface that is usually highly polished.',
 					inputType: 'select',
@@ -176,7 +187,7 @@ angular.module('app')
 						{ label: '2 – smudging present', value: 2 }
 					]
 				}, {
-					model: null,
+					id: null,
 					label: 'Interior surface treatment',
 					description: '',
 					inputType: 'select',
@@ -192,7 +203,7 @@ angular.module('app')
 						{ label: '99 – other – specify in notes', value: 99 },
 					]
 				}, {
-					model: null,
+					id: null,
 					label: 'Sooting',
 					description: 'This variable refers to the presence or absence of sooting, defined as a dark carbon residue.',
 					inputType: 'select',
@@ -208,7 +219,7 @@ angular.module('app')
 						{ label: '5 – no sooting present', value: 5 }
 					]
 				}, {
-					model: null,
+					id: null,
 					label: 'Vessel wall thickness',
 					description: 'This variable refers to the thickness of the thickest portion of the sherd. This is measured using the digital calipers. Do not measure this variable on rim or base sherds. Average of 3 measurements.',
 					inputType: 'number',
@@ -218,7 +229,7 @@ angular.module('app')
 					options: [],
 					unit: 'cm'
 				}, {
-					model: null,
+					id: null,
 					label: 'Width of indentations',
 					description: 'This variable refers to the width of indentations at the widest point. This is measured using the digital calipers. Three indentations are measured for each sherd which will later be averaged.',
 					inputType: 'number',
@@ -228,7 +239,7 @@ angular.module('app')
 					options: [],
 					unit: 'cm'
 				}, {
-					model: null,
+					id: null,
 					label: 'Depth of indentations',
 					description: 'This variable refers to the difference between the deepest portion of an indentation and the top of the adjacent coil. Three indentations are measured which will later be averaged. This is measured using the digital depth gauge.',
 					inputType: 'number',
@@ -238,7 +249,7 @@ angular.module('app')
 					options: [],
 					unit: 'cm'
 				}, {
-					model: null,
+					id: null,
 					label: 'Coil width',
 					description: 'This variable provides an estimate of the average size of coils for each sherd. This is the average of three measures from coil juncture to coil juncture.',
 					inputType: 'number',
@@ -248,7 +259,7 @@ angular.module('app')
 					options: [],
 					unit: 'cm'
 				}, {
-					model: null,
+					id: null,
 					label: 'Number of indentations per sq cm',
 					description: 'This variable refers to the number of indentations per square cm of vessel surface. This is measured by placing the 3x3 cm cardboard cutout over a sherd and recording the number of indentations that are fully visible. If measuring a zoned or patterned corrugated sherd, make sure that unindented portions of the vessel are not visible through the cardboard cutout.',
 					inputType: 'number',
@@ -258,7 +269,7 @@ angular.module('app')
 					options: [],
 					unit: 'per sq cm'
 				},{
-					model: null,
+					id: null,
 					label: 'Proportion of obliterated coils',
 					description: 'This variable refers to the proportion of coils that are obliterated. Obliteration refers to the smoothing of coil junctures so that they are only visible through the indentations. This variable is measured by counting the total number of coils and obliterated coils visible.',
 					inputType: 'number',
@@ -268,7 +279,7 @@ angular.module('app')
 					options: [],
 					unit: ''
 				},{
-					model: null,
+					id: null,
 					label: 'Number of indentations per sq cm',
 					description: 'This variable refers to the number of indentations per square cm of vessel surface. This is measured by placing the 3x3 cm cardboard cutout over a sherd and recording the number of indentations that are fully visible. If measuring a zoned or patterned corrugated sherd, make sure that unindented portions of the vessel are not visible through the cardboard cutout.',
 					inputType: 'number',
@@ -282,7 +293,7 @@ angular.module('app')
 				sectionTitle: 'Only measured on rim sherds',
 				expandedDisplay: false,
 				questions: [{
-					model: null,
+					id: null,
 					label: 'Rim radius',
 					description: 'This variable refers to the radius of the vessel opening. This is measured using the rim radius template chart.',
 					inputType: 'number',
@@ -292,7 +303,7 @@ angular.module('app')
 					options: [],
 					unit: 'per sq cm'
 				}, {
-					model: null,
+					id: null,
 					label: 'Distance to coils',
 					description: 'This variable refers to the distance from the top of the rim to the first exposed coil. This is measured using a flexible rule.',
 					inputType: 'number',
@@ -302,7 +313,7 @@ angular.module('app')
 					options: [],
 					unit: 'per sq cm'
 				}, {
-					model: null,
+					id: null,
 					label: 'Rim form',
 					description: 'This variable refers to the general form of the rim in cross-section. Draw rim on back of form.',
 					inputType: 'select',
@@ -319,7 +330,7 @@ angular.module('app')
 					],
 					unit: 'per sq cm'
 				}, {
-					model: null,
+					id: null,
 					label: 'Rim radius',
 					description: 'This variable refers to the radius of the vessel opening. This is measured using the rim radius template chart.',
 					inputType: 'number',
@@ -333,7 +344,7 @@ angular.module('app')
 				sectionTitle: 'Only measured on basal sherds',
 				expandedDisplay: false,
 				questions: [{
-					model: null,
+					id: null,
 					label: 'Direction of coils',
 					description: 'This variable refers to the direction that coils when looking at the bottom of the vessel from the exterior.',
 					inputType: 'select',
@@ -351,7 +362,7 @@ angular.module('app')
 				sectionTitle: 'Any other observations',
 				expandedDisplay: true,
 				questions: [{
-					model: null,
+					id: null,
 					label: 'Notes',
 					inputType: 'textarea',
 					disabled: false,
@@ -381,10 +392,21 @@ angular.module('app')
 			};
 
 			$scope.cancel = function () {
-				
+
 				//possibly pop a confirmation window for this
 				$scope.formData = {};
 				$state.go('dashboard');
 			};
+
+			$scope.initialize = function () {
+				if (navigator.geolocation) {
+					navigator.geolocation.getCurrentPosition(function (position) {
+						$scope.location.lat = position.coords.latitude;
+						$scope.location.lng = position.coords.longitude;
+					});
+				}
+			};
+
+			$scope.initialize();
 		}
 	]);
