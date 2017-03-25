32bc6c34c1829c1a6d99fdb0af3adbe8cfa5b357
Emily Kauffman <kauffmanes@gmail.com>
2017-03-25 19:04:16 -0400

    updated sql script and find POST

--- api/db/cibola-main.sql
+++ api/db/cibola-main.sql
@@ -7,36 +7,39 @@ DROP TABLE IF EXISTS tbl_finds;
 DROP TABLE IF EXISTS tbl_sites;
 
 CREATE TABLE tbl_sites (
-	SID INT NOT NULL AUTO_INCREMENT,
+    SID INT NOT NULL AUTO_INCREMENT,
     site_name VARCHAR(100) NOT NULL,
-    PRIMARY KEY (SID)
+    CONSTRAINT pk_sid PRIMARY KEY (SID)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE tbl_finds (
-	FID INT NOT NULL AUTO_INCREMENT,
-	find_id VARCHAR(20) NOT NULL, -- ex. ZUNI123, client-facing
+    FID INT NOT NULL AUTO_INCREMENT,
+    find_id VARCHAR(20) NOT NULL, -- ex. ZUNI123, client-facing
     SID INT NOT NULL,
     date_collected DATETIME NOT NULL,
-	PRIMARY KEY (FID),
-    FOREIGN KEY (SID) REFERENCES tbl_sites(SID)
+    CONSTRAINT pk_fid PRIMARY KEY (FID),
+    CONSTRAINT fk_sid FOREIGN KEY (SID) REFERENCES tbl_sites(SID)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 INSERT INTO tbl_sites (site_name) VALUES
-	('Zuni'),
+    ('Zuni'),
     ('Chaco'),
     ('Mesa Verde'),
     ('Clearwater');
     
 INSERT INTO tbl_finds (find_id, SID, date_collected) VALUES
-	('20161220-10', 1, NOW()),
+    ('20161220-10', 1, NOW()),
     ('20161229-12', 1, NOW()),
     ('20161229-11', 2, NOW()),
     ('20161230-33', 3, NOW()),
     ('20161231-14', 2, NOW());
 
+-- Queries to be used in the app
 -- Dashboard View
 CREATE OR REPLACE VIEW vw_dashboard_finds AS
 SELECT f.find_id, s.site_name, f.date_collected
 FROM tbl_finds AS f
 INNER JOIN tbl_sites s
 ON f.SID = s.SID;
+
+SELECT * FROM vw_dashboard_finds;
--- api/routers/api.js
+++ api/routers/api.js
@@ -1,31 +1,34 @@
 var express = require('express');
 var mysql = require('mysql');
 
+var DB_CONNECT_ERR = { "status": 500, "status": "Error connecting to database." };
+var NOT_FOUND = { "status": 404, "statusText": "Resource not found." };
+var BAD_REQUEST = { "status": 400, "statusText": "The client created  bad request." };
+
+//using pool instead of connect to allow multiple threads
 var pool = mysql.createPool({
 	connectionLimit: 100, //important - no crashy
 	host     : 'localhost',
-	user     : '<user here>', //I use root because I suck
-	password : '<password_here>',
+	user     : 'root', //I use root because I suck
+	password : 'x273vrZZ!',
 	database : 'db_cibola',
 	debug    :  false
 });
 
 function handleDatabase (req, res) {
 
-	console.log('Trying to connect!');
-
 	pool.getConnection(function (err, connection) {
 
 		//if errors out on connection, return 
 		if (err) {
-			res.json({ "code": 500, "status": "Error in database connection." });
+			res.json(DB_CONNECT_ERR);
 			return;
 		}
 
 		console.log("Connected as ID " + connection.threadId);
 
 		connection.on('error', function (err) {
-			res.json({ "code": 500, "status": "Error in database connection" });
+			res.json(DB_CONNECT_ERR);
 			return;
 		});
 
@@ -42,14 +45,14 @@ apiRouter.get('/', function(req, res) {
 	handleDatabase(req, res);
 });
 
-//Dashboard REST API
+//GET all finds - Dashboard view
 apiRouter.get('/finds', function (req, res) {
 
 	pool.getConnection(function (err, connection) {
 
 		//if errors out on connection, return 
 		if (err) {
-			res.json({ "code": 500, "status": "Error in database connection." });
+			res.status(500).json(DB_CONNECT_ERR);
 			return;
 		}
 
@@ -58,7 +61,7 @@ apiRouter.get('/finds', function (req, res) {
 			connection.release();
 			
 			if (err) {
-				res.json({ "code": 500, "status": "Error in database connection." });
+				res.status(500).json(DB_CONNECT_ERR);
 				return;
 			}
 
@@ -67,13 +70,52 @@ apiRouter.get('/finds', function (req, res) {
 		});
 
 		connection.on('error', function (err) {
-			res.json({ "code": 500, "status": "Error in database connection" });
+			res.json(DB_CONNECT_ERR);
 			return;
 		});
 
+	});
+});
+
+apiRouter.post('/finds', function (req, res) {
+	
+	//SET to be insert into DB
+	var find = {
+		find_id: req.body.find_id || null,
+		SID: req.body.SID || null,
+		date_collected: req.body.date_collected
+	};
+
+	//if not all the required params exist, throw error
+	if (!find.find_id || !find.SID) {
+		return res.status(400).json(BAD_REQUEST);
+	}
+
+	pool.getConnection(function (err, connection) {
+
+		if (err) {
+			res.json(DB_CONNECT_ERR);
+			return;
+		}
+
+		connection.query("INSERT INTO tbl_finds SET ?", find, function (err, rows) {
+			
+			connection.release();
+
+			return res.status(200).json(rows);
+
+		});
+
+		connection.on('error', function (err) {
+			res.json(DB_CONNECT_ERR);
+			return;
+		});
 
 	});
 
 });
+
+//UPDATE existing record
+apiRouter.route('/finds/id/:find_id');
 
 module.exports = apiRouter;
