6f76b47a19b74eddb617f86a24c0a042183f2339
Emily Kauffman <kauffmanes@gmail.com>
2017-03-25 13:55:07 -0400

    added connection to db

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var port = process.env.PORT || 8080;
+
+//import router for our public REST API
+var apiRouter  = require('./api/routers/api')
+
+//configure our app to handle CORS requests
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+	next();
+});
+
+//Log all request to console
+app.use(morgan('dev'));
+
+//handle application/json content type
+app.use(bodyParser.json());
+
+//route for the web view of the app
+app.use(express.static(__dirname + '/public/'))
+
+//set up api route
+app.use('/api', apiRouter);
+
+//start the server
+app.listen(port);
+
+console.log('Starting server at port ' + port);
